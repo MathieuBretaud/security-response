cfc8a8cfef3b764f1543d50d641f434dffa10773
Olivier <olivier@ogasphere.com>
2021-12-01 16:39:38 +0100

    fix  merge

--- assets/registration.js
+++ assets/registration.js
@@ -1,5 +1,5 @@
 
-console.log('Je suis la!')
+
 const userMemberFormPart = document.getElementById('registration_form_userMember');
 
 //const userMembershipTypeChoices = document.getElementById('choice-register').querySelectorAll('div');
@@ -32,12 +32,19 @@ const userEnterpriseFormPart = document.getElementById('registration_form_userEn
 
     init: function () {
         console.log('Je suis la!')
+        let searchParams = new URLSearchParams(window.location.search);
+        console.log(searchParams);
+        if (searchParams.has('role')) {
+            let param = searchParams.get('sent');
+            console.log('param = ',param);
+        }
+
         const userMemberFormPart = document.getElementById('registration_form_userMember');
         const userEnterpriseFormPart = document.getElementById('registration_form_userEnterprise');
         
         var param="<?php Print($_GET['maVaribleSurlURL']); ?>";
-        console.log('param = ',param);
-        if(param=== 'enterprise') {
+        
+        if(param === 'enterprise') {
             userMemberFormPart.style.display = 'none';
             // TODO: Montrer les champs de l'entreprise
             userEnterpriseFormPart.style.display = 'block';
