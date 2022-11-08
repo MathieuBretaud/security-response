7ffc0fffadda10a7e11145360ef5fe354fd0f607
MathieuBretaud <mathieu.bretaud@gmail.com>
2022-11-08 22:25:31 +0100

    add user crud admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Announcement;
+use App\Entity\Answer;
+use App\Entity\Category;
+use App\Entity\Certification;
+use App\Entity\Company;
+use App\Entity\Enterprise;
+use App\Entity\Member;
+use App\Entity\Specialization;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,5 +36,13 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Announcement', 'fas fa-newspaper', Announcement::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-newspaper', Category::class);
+        yield MenuItem::linkToCrud('Company', 'fas fa-newspaper', Company::class);
+        yield MenuItem::linkToCrud('Enterprise', 'fas fa-newspaper', Enterprise::class);
+        yield MenuItem::linkToCrud('Answer', 'fas fa-newspaper', Answer::class);
+        yield MenuItem::linkToCrud('Member', 'fas fa-newspaper', Member::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-newspaper', User::class);
+        yield MenuItem::linkToCrud('Certification', 'fas fa-newspaper', Certification::class);
+        yield MenuItem::linkToCrud('Specialization', 'fas fa-newspaper', Specialization::class);
     }
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +22,28 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters->add(TextFilter::new('email', 'Email'));
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            yield EmailField::new('email', 'Email'),
+            yield TextField::new('username', 'Pseudo'),
+            yield ArrayField::new('roles'),
+            yield TextField::new('password', 'Mot de passe'),
+            yield AssociationField::new('userMember', 'Membre')
+                ->setCrudController(MemberCrudController::class),
+            yield AssociationField::new('userEnterprise', 'Entreprise')
+                ->setCrudController(EnterpriseCrudController::class),
+            yield AssociationField::new('userCompany', 'Company')
+                ->setCrudController(CategoryCrudController::class),
+            yield BooleanField::new('is_verified', 'Vérifié'),
+
+
         ];
     }
-    */
 }
